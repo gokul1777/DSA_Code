f175f3bfc0ba2eca64c08d6bbd7e56bac4f379ee
Gokul <gokulmehta1777@gmail.com>
2023-08-27 22:13:34 +0530

    Fahrenheit to Celsius programme ---> code FTC

---
+++ Fehrenheit_to_Celsius.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float fehrenheit;
+    cin >> fehrenheit;
+    float celsius = (5.0/9) * (fehrenheit -32);
+    cout << fehrenheit << " F = " << celsius << " C" << endl;
+
+    return 0;
+}
